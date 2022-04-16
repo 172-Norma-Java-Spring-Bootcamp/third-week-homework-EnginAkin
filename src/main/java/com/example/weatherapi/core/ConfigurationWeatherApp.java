dd66ab507d741ed2aaca3363bf76f0afde37d0c6
Engin <enginakin6363@gmail.com>
2022-04-16 13:48:45 +0300

    update

--- src/main/java/com/example/weatherapi/core/ConfigurationWeatherApp.java
+++ src/main/java/com/example/weatherapi/core/ConfigurationWeatherApp.java
@@ -2,10 +2,11 @@ package com.example.weatherapi.core;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+@Configuration
+public class ConfigurationWeatherApp {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
